a7fd5ac3e3284886d06c5d29d4febc913e687416
guillaumereg <guillaumereg@hotmail.fr>
2017-10-10 21:47:39 +0200

    voilà voilà

--- sender.c
+++ sender.c
@@ -4,4 +4,24 @@
 
 int main(int argc, char **argv){
   printf("hello from sender\n");
+
+  struct record * r;
+  unsigned int type = 0;
+  unsigned int tr = 0;
+  unsigned int window = 0;
+  unsigned int seqnum = 0;
+  unsigned int length = 0;
+  unsigned int timestamp = 0;
+  unsigned int crc1 = 0;
+  unsigned char * payload = 0;
+  unsigned int crc2 = 0;
+
+  int a = record_init(r,type,tr,window,seqnum,length,timestamp,crc1,payload,crc2);
+  if (a==-1){
+    return -1;
+  }
+
+  record_free(r);
+
+
 }
