a0b0d7f8e6aff614cb4188ca973dd2f7bce43a70
guillaume <guillaume.neirinckx@student.uclouvain.be>
2017-10-12 16:45:12 +0200

    arguments terminal

--- receiver.c
+++ receiver.c
@@ -1,9 +1,36 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
 #include "paquet.h"
 
 int main(int argc, char **argv){
-  printf("hello from receiver\n");
+
+
+    int option = 0;
+    int filePresent = 0;
+    char* filename = NULL;
+    char* node;  //www.qqch.com ou IPv6
+    char* service; //http ou numero port
+
+    while ((option = getopt(argc, argv,"f:")) != -1) {
+      switch (option) {
+        case 'f' : filename = optarg;
+          filePresent = 1;
+          break;
+        default: break;
+        }
+    }
+    if(filePresent == 1){
+      printf("end getopt, filePresent: %d filename: %s\n", filePresent, filename);
+      node = *(argv+3);
+      service = *(argv+4);
+    }
+    else{
+      node = *(argv+1);
+      service = *(argv+2);
+    }
+    printf("node: %s service: %s\n", node, service);
+
 }
--- sender.c
+++ sender.c
@@ -6,6 +6,7 @@
 #include "paquet.h"
 
 int main(int argc, char **argv){
+  /*
   struct record *r;
   r = (struct record*)malloc(sizeof(struct record));
   int a = record_init(r);
@@ -15,51 +16,41 @@ int main(int argc, char **argv){
   record_set_payload(r,"12345\0",6);
   record_free(r);
 
-  int c;
-  int num=0;
-  int type=0;
-  int length=0;
-  int tr=0;
+  */
+  /*
+  terminal: ./sender 1 2 3 > text.txt
+  printf("hello %s \n", *(argv+3));
 
-  while ( (c = getopt(argc, argv, "t:T:s:l:")) != -1 ) {
-      switch ( c ) {
-          case 't'://type
-            if(atoi(optarg) != 2 && atoi(optarg) !=3){
-                c=-1;
-            }
-            else if (atoi(optarg) ==2){
-              type = 2;
-            }
-            else if (atoi(optarg) ==3){
-              type = 3;
-            }
-          case 'T'://tr
-            if(atoi(optarg) !=0){
-                c=-1;
-            }
-          case 's'://seqnum
-            if(type == 2){
-              num = atoi(optarg) +1;
-            }
-            else   if(type == 2){
-              num = atoi(optarg);
-            }
-            if(num > 255){
-              num=0;
-            }
-          case 'l'://length
-            if( atoi(optarg) > 512){
-              c=-1;
-            }
-            else{
-              length = atoi(optarg);
-            }
+  terminal: ./sender < text.txt
+  int ch;
+    do {
+      ch = fgetc(stdin);
+      putchar(ch);
+  } while (ch != EOF
+  */
+
+  int option = 0;
+  int filePresent = 0;
+  char* filename = NULL;
+  char* node;  //www.qqch.com ou IPv6
+  char* service; //http ou numero port
+
+  while ((option = getopt(argc, argv,"f:")) != -1) {
+    switch (option) {
+      case 'f' : filename = optarg;
+        filePresent = 1;
+        break;
+      default: break;
       }
-    }
+  }
+  if(filePresent == 1){  //position fixe = argv[1] ?
+    node = *(argv+3);
+    service = *(argv+4);
+  }
+  else{
+    node = *(argv+1);
+    service = *(argv+2);
+  }
 
-    //puis un truc de ce style?
-
-    record_set_seqnum(r,num);
-    record_set_type(r,1);
-    //etc
+  return 0;
 }
